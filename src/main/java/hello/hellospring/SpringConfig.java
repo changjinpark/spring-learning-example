1bfb873910128c3f5641398ae6a8a71fc9e82884
changjin.park <pcjin118@gmail.com>
2021-02-13 17:16:02 +0900

    JPA
    - 스프링 부트에 JPA 설정 추가
    - JPA 엔티티 매핑
    - JPA 회원 리포지토리
    - 서비스 계층에 트랜잭션 추가
    - JPA를 사용하도록 스프링 설정 변경:

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -7,15 +7,26 @@ import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    /*
     private final DataSource dataSource;
 
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+    */
+
+    private final DataSource dataSource;
+
+    private final EntityManager em;
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -27,7 +38,8 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 
         //return new MemoryMemberRepository();
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
